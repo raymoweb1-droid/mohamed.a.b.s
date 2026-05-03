06869124513de8a5f3e883a3d3bc597240096904
raymoweb1-droid <raymoweb1@gmail.com>
2026-05-03 23:58:14 +0100

    chore: add supabase anon key

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Put your Supabase details here
 const SUPABASE_URL = "YOUR_SUPABASE_URL_HERE";
-const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY_HERE";
+const SUPABASE_ANON_KEY = "sb_publishable_qJPgdkQJrN7sk_ci2wIbPg_d6IBOChg";
 
 // Initialize Supabase Client
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
