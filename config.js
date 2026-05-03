e636cd19158c822de0df240d1cde24b8624ebcc9
raymoweb1-droid <raymoweb1@gmail.com>
2026-05-04 00:08:38 +0100

    fix: update correct supabase jwt anon key and url

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Put your Supabase details here
-const SUPABASE_URL = "YOUR_SUPABASE_URL_HERE";
-const SUPABASE_ANON_KEY = "sb_publishable_qJPgdkQJrN7sk_ci2wIbPg_d6IBOChg";
+const SUPABASE_URL = "https://egvhekzpkvttmhwiwren.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVndmhla3pwa3Z0dG1od2locmVuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4MzY4MzYsImV4cCI6MjA5MzQxMjgzNn0.7qUU8HxN7q8tQNtJn1mIOj03Mr_EbjIAE0_lMkgUV6g";
 
 // Initialize Supabase Client
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
